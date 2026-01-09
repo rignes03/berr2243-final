b644e8f522d13bcd968337a19f2bf907850d51be
Rignes Tamil <b122320069@student.utem.edu.my>
2026-01-09 16:57:31 +0800

    Fix package.json versions and switch to bcryptjs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
